a18df734b031fdde75e85e9434696290fa47448f
Mo Silvera <mo.leslie.silvera@gmail.com>
2020-05-13 21:28:22 -0500

    practice one

--- Program.cs
+++ Program.cs
@@ -6,7 +6,46 @@ namespace Planner
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Building FiveHundredFifth = new Building("500 5th Avenue N");
+            Building NineteenTwentyTwo = new Building(" 1922 b 14th ave n");
+            Building TwoTwoFourTwo = new Building("2242 tired of coding circle")
         }
     }
+
+    public class Building
+    {
+        private string _designer {get; set;}
+        private DateTime _dateConstructed {get; set;}
+
+        private string _address {get; set;}
+
+        private string _owner {get; set;}
+
+        public int stories {get; set;}
+
+         public double Width {get; set;}
+
+         public double Depth {get; set;}
+
+         public double Volume(){
+             return Width * Depth * (3 * stories);
+         }
+
+         public Building(string address){
+             _address = address;
+         }
+
+         public void Construct() {
+             _dateConstructed = DateTime.Now;
+         }
+
+         public void Purchase(string newOwner) {
+             _owner = newOwner;
+         }
+
+
+
+    }
+
+
 }
