e053700461732676425e00e46ab003099dfc3bba
Mo Silvera <mo.leslie.silvera@gmail.com>
2020-05-13 22:28:06 -0500

    urban planner part one complete

--- Program.cs
+++ Program.cs
@@ -6,9 +6,40 @@ namespace Planner
     {
         static void Main(string[] args)
         {
-            Building FiveHundredFifth = new Building("500 5th Avenue N");
-            Building NineteenTwentyTwo = new Building(" 1922 b 14th ave n");
-            Building TwoTwoFourTwo = new Building("2242 tired of coding circle")
+            Building FiveHundredFifth = new Building("500 5th Avenue N", "Your Mom"){
+                Width= 13.3,
+                Height= 432,
+                stories = 33,
+                Depth = 22
+            };
+            Building NineteenTwentyTwo = new Building(" 1922 b 14th ave n", "Steve Brownlee"){
+                Width= 19.3,
+                Height= 324,
+                stories = 2,
+                Depth = 22
+            };
+            Building TwoTwoFourTwo = new Building("2242 tired of coding circle", "I don't care"){
+                Width= 199.3,
+                Height= 3432,
+                stories = 44,
+                Depth = 22
+            };
+
+
+            FiveHundredFifth.Construct();
+            NineteenTwentyTwo.Construct();
+            TwoTwoFourTwo.Construct();
+
+            FiveHundredFifth.Purchase("Your other mom");
+            NineteenTwentyTwo.Purchase("Someone rich");
+            TwoTwoFourTwo.Purchase("Mr. Moneybags");
+
+            FiveHundredFifth.BuildingReport();
+            NineteenTwentyTwo.BuildingReport();
+            TwoTwoFourTwo.BuildingReport();
+
+
+
         }
     }
 
@@ -25,14 +56,17 @@ namespace Planner
 
          public double Width {get; set;}
 
+        public double Height {get; set;}
          public double Depth {get; set;}
 
          public double Volume(){
-             return Width * Depth * (3 * stories);
+             double totalVolume = Width * Depth * (3 * stories);
+             return totalVolume;
          }
 
-         public Building(string address){
+         public Building(string address, string designer){
              _address = address;
+             _designer = designer;
          }
 
          public void Construct() {
@@ -43,6 +77,16 @@ namespace Planner
              _owner = newOwner;
          }
 
+        public void BuildingReport(){
+            Console.WriteLine(this._address);
+            Console.WriteLine("----------------");
+            Console.WriteLine($"Designed by: {this._designer}");
+            Console.WriteLine($"Constructed on: {this._dateConstructed}");
+            Console.WriteLine($"Owned By: {this._owner}");
+            Console.WriteLine(Volume());
+            Console.WriteLine(" ");
+
+        }
 
 
     }
